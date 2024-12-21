795e1c42e6d5af36b03a2f2affffb4782961a219
Shohorab Hossain Shawon <shohorab.swn@gmail.com>
2024-12-21 21:48:21 +0600

    Bug fix and update

--- components/AboutContents.jsx
+++ components/AboutContents.jsx
@@ -17,13 +17,7 @@ const AboutContents = () => {
           <div className="md:w-1/2 lg:w-2/3 mb-8 md:mb-0 mx-4">
             <TypingAnimation
               className="font-inter text-[18px] sm:text-[20px] md:text-[22px] lg:text-[24px] text-justify font-bold text-black dark:text-white"
-              text="I am a dedicated front-end developer with a strong interest in UI/UX 
-                design, focused on creating intuitive and visually compelling digital 
-                experiences. While still gaining experience in the field, I am 
-                committed to refining my skills and contributing to projects that 
-                prioritize user-centric design and functionality. Driven by a passion 
-                for continuous learning, I am eager to collaborate and deliver 
-                impactful solutions in web development and design."
+              text="I like front-end development and have a solid understanding of digital marketing management. With a keen interest in UI/UX design, I focus on creating user-friendly and visually appealing digital experiences. While still gaining experience in the field, I am committed to enhancing my skills and contributing to projects that emphasize user-centric design and functionality. Driven by a passion for continuous learning, I am eager to collaborate and deliver impactful solutions that integrate both web development and digital marketing strategies."
             />
           </div>
         </div>
--- components/FlipW.jsx
+++ components/FlipW.jsx
@@ -3,16 +3,15 @@ import { FlipWords } from '@/components/ui/flip-words';
 
 export function FlipW() {
   const words = [
+    'Digital Marketing Manager',
     'Front-end Developer',
     'UI/UX Designer',
-    'Digital Marketing Manager',
     'Photographer',
   ];
 
   return (
     <div className="h-[80px] flex justify-center items-center px-4">
       <div className="font-mono text-xl sm:text-1xl md:text-2xl lg:text-3xl xl:text-4xl px-2 mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-        A Passionate
         <FlipWords words={words} /> <br />
       </div>
     </div>
