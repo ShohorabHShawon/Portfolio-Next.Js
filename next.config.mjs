f61d19ecda704b6fdd9dfc0ba9743ebaf3c6cfa2
Shohorab Hossain Shawon <shohorab.swn@gmail.com>
2025-07-07 22:12:03 +0600

    image domains added

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,27 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ['images.unsplash.com', 'secure.notion-static.com'],
+    domains: [
+      'images.unsplash.com',
+      'secure.notion-static.com',
+      'i.imgur.com',
+      'images.unsplash.org',
+      'cdn.sanity.io',
+      'notion.so',
+      'pbs.twimg.com',
+      'res.cloudinary.com',
+      'images.pexels.com',
+      'cdn.pixabay.com',
+      'source.unsplash.com',
+      'via.placeholder.com',
+      'dummyimage.com',
+      'placekitten.com',
+      'loremflickr.com',
+      'fakeimg.pl',
+      'placeholder.com',
+      'placehold.it',
+      'picsum.photos',
+    ],
   },
 };
 
